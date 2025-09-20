768a2b34e35fccb957e558d00bc1ffd3a2e2e147
autumnsroundy <rou24001@byui.edu>
2025-09-20 13:08:19 -0800

    MongoDB connect

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+  console.log(`Running on port ${port}`);
+});
