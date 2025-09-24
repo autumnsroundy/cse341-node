3dcb41125b3b32c42d72648710c7c7ae5ed9d80c
autumnsroundy <rou24001@byui.edu>
2025-09-23 18:00:38 -0800

    Personal Assignment 2 : Rest & MongoDB

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,11 @@ const baseController = require('../controllers/index');
 routes.get('/', baseController.getName);
 
 module.exports = routes;
+
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/contact', require('./contact'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-app.use('/', require('./routes'));
+// Mount routes
+app.use('/', require('./routes'));  // index.js mounts /contacts
 
+// Optional test route to verify server is running
+app.get('/test', (req, res) => {
+  res.send('Server is working!');
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
